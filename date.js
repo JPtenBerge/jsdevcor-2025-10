eab6dc4e0a88d36c13c61156dba05788726783b1
JP <jp@jp.nl>
2025-10-07 10:53:39 +0200

    Datums

--- date.js
+++ date.js
@@ -1,4 +1,3 @@
-
 let nu = new Date();
 
 // nu.getDay
@@ -9,8 +8,8 @@ console.log(nu.getFullYear());
 
 let toen = new Date('2023/04/15');
 let toen2 = new Date('2023-04-15');
-console.log(toen)
-console.log(toen2)
+console.log(toen);
+console.log(toen2);
 
 let toen3 = new Date('2023-02-65');
 console.log(toen3);
